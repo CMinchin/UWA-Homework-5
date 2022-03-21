86e16ce53dc8ffd862ea2a7612a8a7f7febdb21e
Equinox <cameron.minchin@gmail.com>
2022-03-21 21:25:15 +0800

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+var times = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]
+
+var c =  (f) => moment().format(f)
+
+console.log("Today is", c("dddd"), "the", c("Do"), "of", c("MMMM"));
+$(".")
+
+times.forEach(function(e){
+    
+});
