4c4c71077bf7e7b8374a651ae627c79190226e5b
Equinox <cameron.minchin@gmail.com>
2022-03-21 23:23:51 +0800

    done

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,34 @@
 var times = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]
 
-var c =  (f) => moment().format(f)
+var t =  (f) => moment().format(f); // it just make the next line at all readable
 
-console.log("Today is", c("dddd"), "the", c("Do"), "of", c("MMMM"));
-$(".")
+console.log("Today is", t("dddd"), "the", t("Do"), "of", t("MMMM"));
+$("#currentDay").text("Today is " + t("dddd") + " the " + t("Do") + " of "+ t("MMMM"));
 
+var now = "past";
+
+var cal = $(".container")
 times.forEach(function(e){
-    
+    if (t("ha") == e){
+        now = "present";
+    }
+    cal.append(
+        $("<div>").addClass("row").append(
+            $("<div>").addClass("hour col-1").text(e)
+        ).append(
+            $("<div>").addClass("time-block col-11").append(
+                $("<input>").addClass(now).val(window.localStorage.getItem(e))
+            )
+        )
+    );
+    cal[0].children[cal[0].children.length-1].children[1].children[0].id=e;
+    if (t("ha") == e){
+        now = "future";
+    }
+});
+
+$(".saveBtn").click(function(){
+    times.forEach(function(e){
+        window.localStorage.setItem(e,$("#"+e).val());
+    });
 });
